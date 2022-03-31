318d6bea83979a753e14e945631e070ef12c97b7
Suchan mrRO <26240553+tncks@users.noreply.github.com>
2022-04-01 01:04:26 +0900

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Piloom01!@',
+    host: '',
+    user: '',
+    password: '',
     connectionLimit: 5,
-    database: 'memo'
+    database: ''
 });
 
 
